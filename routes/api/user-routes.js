54ff0046424f636385eec87e78a5105cbb8985d1
iiTONELOC <anthonytropeano@me.com>
2021-03-21 18:38:45 -0400

    started post routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,7 +5,7 @@ const { User } = require('../../models');
 // get all users
 router.get('/', (req, res) => {
     User.findAll(
-        // { attributes: { exclude: ['password'] } }
+        { attributes: { exclude: ['password'] } }
     )
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     User.findOne({
-        attributes: { exclude: ['password'] },
+        // attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
         },
@@ -62,14 +62,14 @@ router.post('/', (req, res) => {
         password: req.body.password
     })
         .then(dbUserData => {
-            res.json(dbUserData);
-            // req.session.save(() => {
-            //     req.session.user_id = dbUserData.id;
-            //     req.session.username = dbUserData.username;
-            //     req.session.loggedIn = true;
+            
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
 
-            //     res.json(dbUserData);
-            // });
+                res.json(dbUserData);
+            });
         })
         .catch(err => {
             console.log(err);
@@ -77,52 +77,52 @@ router.post('/', (req, res) => {
         });
 });
 
-// router.post('/login', (req, res) => {
-//     // expects {email: 'lernantino@gmail.com', password: 'password1234'}
-//     User.findOne({
-//         where: {
-//             email: req.body.email
-//         }
-//     }).then(dbUserData => {
-//         if (!dbUserData) {
-//             res.status(400).json({ message: 'No user with that email address!' });
-//             return;
-//         }
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
 
-//         const validPassword = dbUserData.checkPassword(req.body.password);
+        const validPassword = dbUserData.checkPassword(req.body.password);
 
-//         if (!validPassword) {
-//             res.status(400).json({ message: 'Incorrect password!' });
-//             return;
-//         }
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
 
-//         req.session.save(() => {
-//             req.session.user_id = dbUserData.id;
-//             req.session.username = dbUserData.username;
-//             req.session.loggedIn = true;
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });
+});
 
-//             res.json({ user: dbUserData, message: 'You are now logged in!' });
-//         });
-//     });
-// });
-
-// router.post('/logout', (req, res) => {
-//     if (req.session.loggedIn) {
-//         req.session.destroy(() => {
-//             res.status(204).end();
-//         });
-//     }
-//     else {
-//         res.status(404).end();
-//     }
-// });
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
 
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
     // pass in req.body instead to only update what's passed through
     User.update(req.body, {
-        // individualHooks: true,
+        individualHooks: true,
         where: {
             id: req.params.id
         }
--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+//IMPORT SESSIONS
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
