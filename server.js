33d0e3b020436915ad0082dfedf7f63b9b822a16
iiTONELOC <anthonytropeano@me.com>
2021-03-21 22:23:41 -0400

    added handlebars, partials

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 //IMPORT SESSIONS
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -17,8 +20,12 @@ const sess = {
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
 
 // turn on routes
