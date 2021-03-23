997a034b67ebee029d317f696a90b78e12f0f270
iiTONELOC <anthonytropeano@me.com>
2021-03-23 18:34:31 -0400

    added enter listener for signup

--- public/javascript/sign-up.js
+++ public/javascript/sign-up.js
@@ -26,3 +26,38 @@ async function signupFormHandler(event) {
 }
 
 document.querySelector('#sign-up').addEventListener('click', signupFormHandler);
+
+
+function FormHandler(event) {
+
+    if (event.code === "Enter") {
+        myFunction()
+        async function myFunction() {
+            const username = document.querySelector('#username-signup').value.trim();
+            const email = document.querySelector('#email-signup').value.trim();
+            const password = document.querySelector('#password-signup').value.trim();
+
+            if (username && email && password) {
+                const response = await fetch('/api/users', {
+                    method: 'post',
+                    body: JSON.stringify({
+                        username,
+                        email,
+                        password
+                    }),
+                    headers: { 'Content-Type': 'application/json' }
+                });
+
+                // check the response status
+                if (response.ok) {
+                    document.location.replace(`/`);
+                } else {
+                    alert(response.statusText);
+                }
+            }
+        }
+
+    } else { return }
+
+}
+(document).addEventListener('keydown', FormHandler);
