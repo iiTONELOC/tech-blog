c5a800d2ced9a4544c9ebd0d095926db901168a3
iiTONELOC <anthonytropeano@me.com>
2021-07-15 14:47:51 -0400

    removed service filter, see if it will find my phone

--- public/javascript/bluetooth.js
+++ public/javascript/bluetooth.js
@@ -13,7 +13,7 @@ searchDevicesBtn.addEventListener('click', function (event) {
 
     navigator.bluetooth.requestDevice({
         acceptAllDevices: true,
-        optionalServices: ['battery_service'] // Required to access service later.
+
     })
         .then(device => {
             console.log(device);
@@ -21,12 +21,5 @@ searchDevicesBtn.addEventListener('click', function (event) {
         })
         .catch(error => { console.error(error); });
 
-    // navigator.permissions.query({ name: "bluetooth" }).then(status => {
-    //     if (status.state !== 'denied') checkAvailability();
 
-    //     // Bluetooth is blocked, listen for change in PermissionStatus.
-    //     status.onchange = () => {
-    //         if (this.state !== 'denied') checkAvailability();
-    //     };
-    // });
 });
